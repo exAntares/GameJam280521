b3f32f3de3708e4a38ab8c23a6072becb0de2a72
Luis Rojas <luis_andres81@hotmail.com>
2021-05-29 22:08:22 +0200

    Fix sound rising hunting per frame

--- Assets/Scripts/LibrarianTargetTrigger.cs
+++ Assets/Scripts/LibrarianTargetTrigger.cs
@@ -6,19 +6,30 @@ public class LibrarianTargetTrigger : MonoBehaviour {
     [SerializeField] private LibrarianController _librarianController;
     [SerializeField] private string _otherTag;
     [SerializeField] private bool _setAsPlayer;
-    
+
     void Start() {
         this.GetAsyncTriggerStay2DTrigger()
             .ForEachAsync(OnStay2D);
+
+        this.GetAsyncTriggerEnter2DTrigger()
+            .ForEachAsync(OnEnter2D);
     }
 
-    private void OnStay2D(Collider2D obj) {
+    private void OnEnter2D(Collider2D obj) {
         if (obj.CompareTag(_otherTag)) {
             if (_setAsPlayer) {
                 _librarianController.SetPlayerTarget(obj.transform);
             }
             else {
                 _librarianController.SetTarget(obj.transform);
+            }
+        }
+    }
+
+    private void OnStay2D(Collider2D obj) {
+        if (_setAsPlayer) {
+            if (obj.CompareTag(_otherTag)) {
+                _librarianController.SetPlayerTarget(obj.transform);
             }
         }
     }
