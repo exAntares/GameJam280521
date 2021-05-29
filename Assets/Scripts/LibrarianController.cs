c88cd5158a8daf88135109b71116ab15b06b3d44
Luis Rojas <luis_andres81@hotmail.com>
2021-05-29 16:41:54 +0200

    Librarian patrolling

--- Assets/Scripts/LibrarianController.cs
+++ Assets/Scripts/LibrarianController.cs
@@ -10,18 +10,27 @@ public class LibrarianController : MonoBehaviour {
     [SerializeField] private Path _path;
     [SerializeField] private Seeker _seeker;
     [SerializeField] private Rigidbody2D _rigidbody2D;
-    [SerializeField] private Transform _target;
+    //[SerializeField] private Transform _target;
     [SerializeField] private float _speed = 1.0f;
+
+    public Vector3 _targetPosition;
+
+    public void SetTargetPosition(Vector3 targetPos) => _targetPosition = targetPos;
 
     // Start is called before the first frame update
     private void Start() {
+        _targetPosition = transform.position;
         GetPath().Forget();
         UniTaskAsyncEnumerable.EveryUpdate().ForEachAsync(_ => {
             if (_path != null && !_path.error) {
                 if (_path.vectorPath.Count > 1) {
                     var target = _path.vectorPath[1];
-                    var direction = target - transform.position;
-                    var directionNormalized = direction.normalized;
+                    var distance = target - transform.position;
+                    if (distance.sqrMagnitude <= 0.1f) {
+                        return;
+                    }
+                    
+                    var directionNormalized = distance.normalized;
                     var force = directionNormalized * (_speed * Time.deltaTime);
                     if (force.magnitude > 1) {
                         if (force.x > 0) {
@@ -52,7 +61,7 @@ public class LibrarianController : MonoBehaviour {
             taskSource.TrySetResult(p);
         }
 
-        _seeker.StartPath(transform.position, _target.position, OnCalculatePath);
+        _seeker.StartPath(transform.position, _targetPosition, OnCalculatePath);
         return await taskSource.Task;
     }
 }
